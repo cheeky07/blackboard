14e04dd87bbac9f61482d8fa8fe70a6ad3df2e17
cheeky07 <akankshakashyap@yandex.com>
2021-11-30 22:40:29 +0530

    added draw functions

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 import {
     getHeight,
-    getWidth
+    getWidth,
+    Point
 } from "./utils.js";
+
+let pen;
+var last = null;
+var pendown;
 
 function main() {
 
     let canvas = document.getElementById("main_canvas");
     canvas.height = getHeight();
     canvas.width = getWidth();
-    let pen = canvas.getContext('2d');
+    pen = canvas.getContext('2d');
+    pen.fillStyle = "orange";
+    pen.fillRect(0, 0, canvas.width, canvas.height);
+    pen.lineWidth = 5;
+    pen.lineJoin = "round";
+    pen.strokeStyle = "white";
+    canvas.addEventListener('mousedown',penDown);
+    canvas.addEventListener('mousemove',penMove);
+    canvas.addEventListener('mouseup',penUp);
 }
+
+function penDown(event){
+    pen.moveTo(event.pageX, event.pageY);
+    pen.beginPath();
+    pendown = 1;
+}
+
+function penMove(event){
+    if(pendown){
+        pen.lineTo(event.pageX, event.pageY);
+    }
+}
+
+function penUp(event){
+    pendown = 0;
+    pen.stroke();   
+}
+
 
 main();
