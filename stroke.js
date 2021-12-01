c42be2dd1a50933b10c5c17e1d7b7ab409656ef7
cheeky07 <akankshakashyap@yandex.com>
2021-12-01 15:52:59 +0530

    added drawStroke function

--- stroke.js
+++ stroke.js
@@ -2,6 +2,14 @@ class Stroke {
     constructor() {
         this.points = [];
     }
+    drawStroke(pen){
+        if(points.length<2) return;
+        pen.moveTo(points[0].x, points[0].y);
+        for(var i=1; i<points.length; i++){
+            pen.lineTo(points[i].x, points[i].y);
+        }
+        pen.stroke();
+    }
 }
 
 export { Stroke };
