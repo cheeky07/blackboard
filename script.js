48757e853021ba30e3352b414f3c371f9045f15a
Mrinal Agarwal <mrinal13@yandex.com>
2021-12-01 16:06:54 +0530

    added live pen, stroke smoothening

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ import {
     Stroke,
 } from "./stroke.js";
 
-let pen;
+let pen, lpen, height, width;
 var pendown;
 var currentStroke, point;
 
@@ -16,37 +16,41 @@ var currentStroke, point;
 function main() {
 
     let canvas = document.getElementById("main_canvas");
-    canvas.height = getHeight();
-    canvas.width = getWidth();
+    let lcanvas = document.getElementById("live_canvas");
+    height = lcanvas.height = canvas.height = getHeight();
+    width = lcanvas.width = canvas.width = getWidth();
     pen = canvas.getContext('2d');
+    lpen = lcanvas.getContext('2d');
     pen.fillStyle = "orange";
     pen.fillRect(0, 0, canvas.width, canvas.height);
-    pen.lineWidth = 5;
-    pen.lineJoin = "round";
-    pen.strokeStyle = "white";
-    canvas.addEventListener('mousedown', penDown);
-    canvas.addEventListener('mousemove', penMove);
-    canvas.addEventListener('mouseup', penUp);
+    lpen.lineWidth = pen.lineWidth = 5;
+    lpen.lineJoin = pen.lineJoin = "round";
+    lpen.strokeStyle = pen.strokeStyle = "white";
+    lcanvas.addEventListener('mousedown', penDown);
+    lcanvas.addEventListener('mousemove', penMove);
+    lcanvas.addEventListener('mouseup', penUp);
 }
 
 function penDown(event) {
-    pen.moveTo(event.pageX, event.pageY);
     currentStroke = new Stroke();
     currentStroke.points.push(new Point(event.pageX, event.pageY));
-    pen.beginPath();
+    lpen.moveTo(event.pageX, event.pageY);
+    lpen.beginPath();
     pendown = 1;
 }
 
 function penMove(event) {
     if (pendown) {
-        pen.lineTo(event.pageX, event.pageY);
-        pen.stroke();
+        lpen.lineTo(event.pageX, event.pageY);
+        lpen.stroke();
         currentStroke.points.push(new Point(event.pageX, event.pageY));
     }
 }
 
 function penUp(event) {
     pendown = 0;
+    currentStroke.draw(pen);
+    lpen.clearRect(0, 0, width, height);
 }
 
 
